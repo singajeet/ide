0a4a5ab2ca98b5027d03b05b3bf825d777fff708
Ajeet Singh <singajeet@gmail.com>
2018-04-03 22:24:10 +0530

    config changes

--- source/common/config.py
+++ source/common/config.py
@@ -79,3 +79,57 @@ AIML_FILES_OUTPUT_PATH = _config_parser.get(AIML_SECTION,
 AIML_DB_KEY = 'aiml_db'
 AIML_DB = _config_parser.get(AIML_SECTION, AIML_DB_KEY)
 
+################ CLI configuration #############################
+class Token(object):
+    _symbol_key = 'token_symbol'
+    _color_key = 'token_color'
+    Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+    Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class Colon(object):
+        _symbol_key = 'colon_symbol'
+        _color_key = 'colon_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class Pound(object):
+        _symbol_key = 'pound_symbol'
+        _color_key = 'pound_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class Pattern(object):
+        _symbol_key = 'pattern_symbol'
+        _color_key = 'pattern_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class Template(object):
+        _symbol_key = 'template_symbol'
+        _color_key = 'template_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class Default(object):
+        _symbol_key = 'default_symbol'
+        _color_key = 'default_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class LBracket(object):
+        _symbol_key = 'lbracket_symbol'
+        _color_key = 'lbracket_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class RBracket(object):
+        _symbol_key = 'rbracket_symbol'
+        _color_key = 'rbracket_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
+
+    class Command(object):
+        _symbol_key = 'command_symbol'
+        _color_key = 'command_color'
+        Symbol = _config_parser.get(TOKENS_SECTION, _symbol_key)
+        Color =  _config_parser.get(TOKENS_SECTION, _color_key)
